df922a31f3ad00043329c91352faf4c341d4a892
IGOLIVIT <131347905+IGOLIVIT@users.noreply.github.com>
2024-07-31 14:26:38 +0300

    Initial Commit

---
+++ App750/App750App.swift
@@ -0,0 +1,17 @@
+//
+//  App750App.swift
+//  App750
+//
+//  Created by IGOR on 31/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct App750App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
